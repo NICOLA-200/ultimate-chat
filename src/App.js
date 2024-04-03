145cbf6d332ca41c7b1fc32d844232a6390b274c
NICOLA-200 <fatepepe66@gmail.com>
2024-04-03 21:29:58 +0200

    error solved

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignUp.jsx";
 import Login from "./pages/Login.jsx";
 import Profile from "./pages/Profile.jsx";
-import ChatPage from "./pages/ChatPage.jsx";
 import Cookies from "js-cookie";
 import { Store } from "./redux/store.js";
 import { currentUser } from "./redux/actions/user.js";
@@ -32,7 +31,6 @@ function App() {
         <Route path="/signup" Component={isAuthenticated ? Home : SignUp} />
         <Route path="/login" Component={isAuthenticated ? Home : Login} />
         <Route path="/Profile" Component={Profile} />
-        <Route path="/chat" Component={isAuthenticated && ChatPage} />
       </Routes>
     </BrowserRouter>
   );
